7eecf9b57fc2a7fd9f3b023a0b5ad47a8692dcbf
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-08-31 12:11:09 +0600

    Update cpmponents.ts

--- cpmponents.ts
+++ cpmponents.ts
@@ -1,1 +1,22 @@
 
+<!-- в файле Layout.vue -->
+<template>
+  <div>
+    <HeaderComponent />
+    <div class="main-layout">
+      <NavbarComponent />
+      <div class="content">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import HeaderComponent from './HeaderComponent.vue'
+import NavbarComponent from './NavbarComponent.vue'
+</script>
+
+<style>
+/* Ваши стили */
+</style>
