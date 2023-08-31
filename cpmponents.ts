d380001dca0abef2d6df619309746759d732eaad
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-08-31 12:17:52 +0600

    Update cpmponents.ts

--- cpmponents.ts
+++ cpmponents.ts
@@ -1,53 +1,19 @@
 <template>
-  <div class="navbar">
-    <ul>
-      <li>
-        <router-link to="/main">Главная</router-link>
-      </li>
-      <li>
-        <router-link to="/map">Карта</router-link>
-      </li>
-      <li>
-        <router-link to="/orders">Заказы</router-link>
-      </li>
-      <li>
-        <router-link to="/detail">Детали</router-link>
-      </li>
-      <li>
-        <router-link to="/search">Поиск</router-link>
-      </li>
-    </ul>
+  <div id="app">
+    <router-view />
   </div>
 </template>
 
 <script setup>
-// Импорты и логика (если необходимо)
+// Логика скрипта, если требуется
 </script>
 
 <style>
-.navbar {
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  width: 200px;
-  background-color: #333;
-  color: white;
-  overflow: auto;
-}
-
-.navbar ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-
-.navbar li {
-  padding: 8px;
+/* Глобальные стили приложения */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
-}
-
-.router-link-active {
-  background-color: #666;
+  color: #2c3e50;
 }
 </style>
+
