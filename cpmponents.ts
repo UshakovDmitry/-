8c73d917e6f7e78b7a56f35ccff2751f44b0c46a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-08-31 12:13:36 +0600

    Update cpmponents.ts

--- cpmponents.ts
+++ cpmponents.ts
@@ -1,22 +1,53 @@
-
-<!-- в файле Layout.vue -->
 <template>
-  <div>
-    <HeaderComponent />
-    <div class="main-layout">
-      <NavbarComponent />
-      <div class="content">
-        <router-view></router-view>
-      </div>
-    </div>
+  <div class="navbar">
+    <ul>
+      <li>
+        <router-link to="/main">Главная</router-link>
+      </li>
+      <li>
+        <router-link to="/map">Карта</router-link>
+      </li>
+      <li>
+        <router-link to="/orders">Заказы</router-link>
+      </li>
+      <li>
+        <router-link to="/detail">Детали</router-link>
+      </li>
+      <li>
+        <router-link to="/search">Поиск</router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup>
-import HeaderComponent from './HeaderComponent.vue'
-import NavbarComponent from './NavbarComponent.vue'
+// Импорты и логика (если необходимо)
 </script>
 
 <style>
-/* Ваши стили */
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 200px;
+  background-color: #333;
+  color: white;
+  overflow: auto;
+}
+
+.navbar ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.navbar li {
+  padding: 8px;
+  text-align: center;
+}
+
+.router-link-active {
+  background-color: #666;
+}
 </style>
