b7301c2e269dc869cfa6d468ea21bbb08571a2ee
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-04 16:21:48 +0600

    Update test.vue

--- test.vue
+++ test.vue
@@ -1,41 +1,37 @@
-<!-- Pagination.vue -->
-<template>
-  <div class="pagination">
-    <button :disabled="currentPage === 1" @click="changePage(currentPage - 1)">
-      Previous
-    </button>
-    <span>Page {{ currentPage }} of {{ totalPages }}</span>
-    <button :disabled="currentPage === totalPages" @click="changePage(currentPage + 1)">
-      Next
-    </button>
+ <Pagination
+      :total-pages="totalPages"
+      :initial-page="1"
+      @page-change="handlePageChange"
+    />
   </div>
 </template>
 
-<script setup>
-import { ref, defineProps, defineEmits } from "vue";
+<script setup lang="ts">
+import { ref, computed, defineProps, defineEmits } from 'vue';
+import Pagination from './Pagination.vue'; // Путь к вашему компоненту пагинации
+import cellTypeToggle from '../global/cell-collection/cellTypeToggle.vue';
+import cellTypeSimple from '../global/cell-collection/cellTypeSimple.vue';
 
-const props = defineProps({
-  totalPages: Number,
-  initialPage: {
-    type: Number,
-    default: 1,
-  },
+const props = defineProps<{
+  headers: string[];
+  rows: any[];
+  config: any;
+}>();
+
+const itemsPerPage = ref(5); // Количество элементов на странице
+const currentPage = ref(1); // Текущая страница
+
+const totalPages = computed(() => Math.ceil(props.rows.length / itemsPerPage.value));
+
+const paginatedRows = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage.value;
+  const end = start + itemsPerPage.value;
+  return props.rows.slice(start, end);
 });
 
-const currentPage = ref(props.initialPage);
-const emits = defineEmits(["page-change"]);
-
-const changePage = (newPage) => {
-  if (newPage >= 1 && newPage <= props.totalPages) {
-    currentPage.value = newPage;
-    emits("page-change", newPage);
-  }
+const handlePageChange = (newPage: number) => {
+  currentPage.value = newPage;
 };
-</script>
-
-<style scoped>
-/* ... ваш CSS ... */
-</style>
 
 
 
