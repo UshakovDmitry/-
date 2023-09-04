3a14ee3f3e47b40d7303b57ded82cd82ad8d259e
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-04 11:29:20 +0600

    Update test.vue

--- test.vue
+++ test.vue
@@ -2,11 +2,19 @@
   <!-- Контейнер для поля поиска -->
   <div class="search-container">
     <!-- Поле ввода -->
+    <IconComponent
+      :сonfig="{
+        name: 'search',
+        color: '#23362D4D',
+        width: 24,
+        height: 24,
+      }"
+    ></IconComponent>
     <input
       type="text"
       :value="searchValue"
       @input="updateSearch"
-      placeholder="Введите запрос..."
+      :placeholder="props.placeholder"
       class="search-input"
     />
   </div>
@@ -14,7 +22,10 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-
+import IconComponent from '../../../global/icon/icon.component.vue';
+const props = defineProps<{
+  placeholder: string;
+}>();
 // Определяем эмиттеры для событий
 const emits = defineEmits(['onSearch']);
 
@@ -25,29 +36,29 @@ const searchValue = ref('');
 const updateSearch = (event: Event) => {
   // Получаем текущее значение из поля ввода
   const value = (event.target as HTMLInputElement).value;
-  
+
   // Обновляем локальное состояние
   searchValue.value = value;
-  
+
   // Эмитим событие с новым значением для родительского компонента
   emits('onSearch', value);
 };
 </script>
 
 <style scoped>
-  .search-container {
-    /* Стили для контейнера */
-  }
+.search-container {
+  /* Стили для контейнера */
+}
 
-  .search-input {
-    /* Стили для поля ввода */
-    border-radius: 16px;
-    background: #f2f3f5;
-    padding: 12px 16px;
-    font-size: 14px;
-    border: none;
-    outline: transparent;
-    color: hsl(152, 21%, 17%);
-    width: 100%;
-  }
+.search-input {
+  /* Стили для поля ввода */
+  border-radius: 16px;
+  background: #f2f3f5;
+  padding: 12px 16px;
+  font-size: 14px;
+  border: none;
+  outline: transparent;
+  color: hsl(152, 21%, 17%);
+  width: 100%;
+}
 </style>
