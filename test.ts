2a753e0f16f7ee40b11f12c2ae90f76b4099ba5c
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-05 11:46:37 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
-npm install @stomp/stompjs
+runtime-dom.esm-bundler.js:33 Uncaught (in promise) TypeError: Cannot read properties of null (reading 'parentNode')
+    at parentNode (runtime-dom.esm-bundler.js:33:30)
+    at ReactiveEffect.componentUpdateFn [as fn] (runtime-core.esm-bundler.js:5777:11)
+    at ReactiveEffect.run (reactivity.esm-bundler.js:178:19)
+    at instance.update (runtime-core.esm-bundler.js:5814:51)
+    at callWithErrorHandling (runtime-core.esm-bundler.js:158:32)
+    at flushJobs (runtime-core.esm-bundler.js:357:9)
