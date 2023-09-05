82c5333d80d7c9b6ec6e8a6bd5cf285a53c22cb9
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-05 14:54:58 +0600

    Update test.vue

--- test.vue
+++ test.vue
@@ -1,34 +1,163 @@
-<div class="page-btn" @click="goToFirstPage">
-  <IconComponent
-    :сonfig="{
-      name: 'doubleLeft', // Имя вашей иконки для "Первая страница"
-      color: '#23362D4D',
-      width: 24,
-      height: 24,
-    }"
-  ></IconComponent>
-</div>
+<template>
+  <div class="table-wrapper">
+    <table class="table-component" v-if="props.rows.length >= 1">
+      <thead>
+        <tr class="table-thead-tr">
+          <th
+            class="table-thead-tr-th"
+            v-for="(header, index) in props.headers"
+          >
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          class="table-tbody-tr"
+          v-for="(row, rowIndex) in paginatedRows"
+          :key="rowIndex"
+        >
+          <td
+            v-for="(cell, cellIndex, i) in row"
+            :key="cellIndex"
+            class="table-tbody-tr-td"
+          >
+            <cell-type-toggle
+              v-if="props.config.rows[i].config.type === 2"
+              :config="{ ...props.config.rows[i].config, value: cell, id: i }"
+            ></cell-type-toggle>
+            <cell-type-simple
+              v-if="props.config.rows[i].config.type === 1"
+              :config="{ ...props.config.rows[i].config, value: cell, id: i }"
+            ></cell-type-simple>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="paginator__wrapper">
+      <paginator-component
+        :total-pages="totalPages"
+        :initial-page="1"
+        @page-change="handlePageChange"
+      />
+    </div>
+  </div>
+</template>
 
-<!-- Ваши текущие кнопки -->
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import PaginatorComponent from '../global/paginator/paginator.vue';
+import cellTypeToggle from '../global/cell-collection/cellTypeToggle.vue';
+import cellTypeSimple from '../global/cell-collection/cellTypeSimple.vue';
 
-<div class="page-btn" @click="goToLastPage">
-  <IconComponent
-    :сonfig="{
-      name: 'doubleRight', // Имя вашей иконки для "Последняя страница"
-      color: '#23362D4D',
-      width: 24,
-      height: 24,
-    }"
-  ></IconComponent>
-</div>
+const props = defineProps<{
+  headers: string[];
+  rows: any[];
+  config: any;
+}>();
 
+const itemsPerPage = ref(8); // Количество элементов на странице
+const currentPage = ref(1); // Текущая страница
 
+const totalPages = computed(() =>
+  Math.ceil(props.rows.length / itemsPerPage.value),
+);
 
+const paginatedRows = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage.value;
+  const end = start + itemsPerPage.value;
+  return props.rows.slice(start, end);
+});
 
-const goToFirstPage = () => {
-  goToPage(1);
+const handlePageChange = (newPage: number) => {
+  currentPage.value = newPage;
 };
+</script>
 
-const goToLastPage = () => {
-  goToPage(props.totalPages);
-};
+<style scoped>
+::-webkit-scrollbar {
+  width: 10px; /* ширина для вертикального скролла */
+  height: 10px; /* высота для горизонтального скролла */
+  background-color: white;
+  border-radius: 16px;
+}
+::-webkit-scrollbar-thumb {
+  background-color: #c4c9c6;
+  border-radius: 16px;
+}
+.table-wrapper {
+  white-space: nowrap;
+  position: relative;
+  border-radius: 16px;
+  width: 100%;
+  height: auto;
+}
+
+.table-component {
+  border-collapse: collapse;
+  background-color: white;
+  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
+  background: #fff;
+  width: 100%;
+  height: 100%;
+}
+thead {
+  position: sticky;
+  top: 0;
+}
+.table-thead-tr {
+  padding-right: 20px;
+  border-bottom: 1px solid #e4e7e5;
+  background-color: #f1f7f4;
+  border-radius: 80px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05);
+  height: 80px;
+}
+
+.table-thead-tr-th {
+  padding: 10px 20px;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  color: #23362d;
+  border-bottom: 1px solid #e4e7e5;
+  text-align: left;
+}
+.alser-table-header {
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  width: 100%;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+  color: #23362d;
+}
+
+.sort-icon {
+  cursor: pointer;
+  margin: 5px;
+}
+
+.table-tbody-tr {
+  height: 80px;
+}
+.table-tbody-tr-td {
+  /* padding: 10px 20px; */
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  color: #23362d;
+  border-bottom: 1px solid #e4e7e5;
+  text-align: left;
+}
+
+.paginator__wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+</style>
