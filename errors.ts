91ddae818cb736a8df9f947d181ad1ef27c49faf
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-05 11:10:46 +0600

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -1,6 +1,4 @@
-
-transport:1 Access to fetch at 'http://rabbitmq.next.local/api/queues/%2F/TmsQueue/get' from origin 'http://127.0.0.1:5173' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
-transport.viewmodel.ts:27     POST http://rabbitmq.next.local/api/queues/%2F/TmsQueue/get net::ERR_FAILED
+transport.viewmodel.ts:27     POST http://rabbitmq.next.local/api/queues/%2F/TmsQueue/get net::ERR_ABORTED 401 (Unauthorized)
 readFromQueue @ transport.viewmodel.ts:27
 TransportComponentViewModel @ transport.viewmodel.ts:8
 setup @ transport.component.vue:53
@@ -8,6 +6,32 @@ callWithErrorHandling @ runtime-core.esm-bundler.js:158
 setupStatefulComponent @ runtime-core.esm-bundler.js:7236
 setupComponent @ runtime-core.esm-bundler.js:7197
 mountComponent @ runtime-core.esm-bundler.js:5599
+processComponent @ runtime-core.esm-bundler.js:5565
+patch @ runtime-core.esm-bundler.js:5040
+componentUpdateFn @ runtime-core.esm-bundler.js:5708
+run @ reactivity.esm-bundler.js:178
+instance.update @ runtime-core.esm-bundler.js:5814
+setupRenderEffect @ runtime-core.esm-bundler.js:5822
+mountComponent @ runtime-core.esm-bundler.js:5612
+processComponent @ runtime-core.esm-bundler.js:5565
+patch @ runtime-core.esm-bundler.js:5040
+mountChildren @ runtime-core.esm-bundler.js:5284
+mountElement @ runtime-core.esm-bundler.js:5191
+processElement @ runtime-core.esm-bundler.js:5156
+patch @ runtime-core.esm-bundler.js:5028
+mountChildren @ runtime-core.esm-bundler.js:5284
+mountElement @ runtime-core.esm-bundler.js:5191
+processElement @ runtime-core.esm-bundler.js:5156
+patch @ runtime-core.esm-bundler.js:5028
+mountChildren @ runtime-core.esm-bundler.js:5284
+mountElement @ runtime-core.esm-bundler.js:5191
+processElement @ runtime-core.esm-bundler.js:5156
+patch @ runtime-core.esm-bundler.js:5028
+componentUpdateFn @ runtime-core.esm-bundler.js:5708
+run @ reactivity.esm-bundler.js:178
+instance.update @ runtime-core.esm-bundler.js:5814
+setupRenderEffect @ runtime-core.esm-bundler.js:5822
+mountComponent @ runtime-core.esm-bundler.js:5612
 processComponent @ runtime-core.esm-bundler.js:5565
 patch @ runtime-core.esm-bundler.js:5040
 componentUpdateFn @ runtime-core.esm-bundler.js:5773
@@ -37,20 +61,21 @@ finalizeNavigation @ vue-router.mjs:3355
 Promise.then (async)
 pushWithRedirect @ vue-router.mjs:3187
 push @ vue-router.mjs:3112
-navigate @ vue-router.mjs:2201
-callWithErrorHandling @ runtime-core.esm-bundler.js:158
-callWithAsyncErrorHandling @ runtime-core.esm-bundler.js:166
-invoker @ runtime-dom.esm-bundler.js:278
-Show 35 more frames
+install @ vue-router.mjs:3551
+use @ runtime-core.esm-bundler.js:3752
+(anonymous) @ main.ts:13
+Show 59 more frames
 Show less
-transport.viewmodel.ts:45 Error: TypeError: Failed to fetch
-    at TransportComponentViewModel.readFromQueue (transport.viewmodel.ts:27:5)
-    at new TransportComponentViewModel (transport.viewmodel.ts:8:10)
-    at setup (transport.component.vue:53:3)
-    at callWithErrorHandling (runtime-core.esm-bundler.js:158:18)
-    at setupStatefulComponent (runtime-core.esm-bundler.js:7236:25)
-    at setupComponent (runtime-core.esm-bundler.js:7197:36)
-    at mountComponent (runtime-core.esm-bundler.js:5599:7)
-    at processComponent (runtime-core.esm-bundler.js:5565:9)
-    at patch (runtime-core.esm-bundler.js:5040:11)
-    at ReactiveEffect.componentUpdateFn [as fn] (runtime-core.esm-bundler.js:5773:9)
+transport.viewmodel.ts:46 Error: SyntaxError: Unexpected end of input (at transport.viewmodel.ts:41:36)
+    at transport.viewmodel.ts:41:36
+
+
+
+
+
+
+
+
+
+
+401 Unauthorized
