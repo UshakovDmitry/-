720f8b09cef00aec2b99628c5b2acddbf19110f4
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-05 11:13:39 +0600

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -1,81 +1,42 @@
-transport.viewmodel.ts:27     POST http://rabbitmq.next.local/api/queues/%2F/TmsQueue/get net::ERR_ABORTED 401 (Unauthorized)
-readFromQueue @ transport.viewmodel.ts:27
-TransportComponentViewModel @ transport.viewmodel.ts:8
-setup @ transport.component.vue:53
-callWithErrorHandling @ runtime-core.esm-bundler.js:158
-setupStatefulComponent @ runtime-core.esm-bundler.js:7236
-setupComponent @ runtime-core.esm-bundler.js:7197
-mountComponent @ runtime-core.esm-bundler.js:5599
-processComponent @ runtime-core.esm-bundler.js:5565
-patch @ runtime-core.esm-bundler.js:5040
-componentUpdateFn @ runtime-core.esm-bundler.js:5708
-run @ reactivity.esm-bundler.js:178
-instance.update @ runtime-core.esm-bundler.js:5814
-setupRenderEffect @ runtime-core.esm-bundler.js:5822
-mountComponent @ runtime-core.esm-bundler.js:5612
-processComponent @ runtime-core.esm-bundler.js:5565
-patch @ runtime-core.esm-bundler.js:5040
-mountChildren @ runtime-core.esm-bundler.js:5284
-mountElement @ runtime-core.esm-bundler.js:5191
-processElement @ runtime-core.esm-bundler.js:5156
-patch @ runtime-core.esm-bundler.js:5028
-mountChildren @ runtime-core.esm-bundler.js:5284
-mountElement @ runtime-core.esm-bundler.js:5191
-processElement @ runtime-core.esm-bundler.js:5156
-patch @ runtime-core.esm-bundler.js:5028
-mountChildren @ runtime-core.esm-bundler.js:5284
-mountElement @ runtime-core.esm-bundler.js:5191
-processElement @ runtime-core.esm-bundler.js:5156
-patch @ runtime-core.esm-bundler.js:5028
-componentUpdateFn @ runtime-core.esm-bundler.js:5708
-run @ reactivity.esm-bundler.js:178
-instance.update @ runtime-core.esm-bundler.js:5814
-setupRenderEffect @ runtime-core.esm-bundler.js:5822
-mountComponent @ runtime-core.esm-bundler.js:5612
-processComponent @ runtime-core.esm-bundler.js:5565
-patch @ runtime-core.esm-bundler.js:5040
-componentUpdateFn @ runtime-core.esm-bundler.js:5773
-run @ reactivity.esm-bundler.js:178
-instance.update @ runtime-core.esm-bundler.js:5814
-callWithErrorHandling @ runtime-core.esm-bundler.js:158
-flushJobs @ runtime-core.esm-bundler.js:357
-Promise.then (async)
-queueFlush @ runtime-core.esm-bundler.js:270
-queuePostFlushCb @ runtime-core.esm-bundler.js:290
-queueEffectWithSuspense @ runtime-core.esm-bundler.js:1603
-scheduler @ runtime-core.esm-bundler.js:1773
-triggerEffect @ reactivity.esm-bundler.js:373
-triggerEffects @ reactivity.esm-bundler.js:363
-triggerRefValue @ reactivity.esm-bundler.js:974
-(anonymous) @ reactivity.esm-bundler.js:1135
-triggerEffect @ reactivity.esm-bundler.js:373
-triggerEffects @ reactivity.esm-bundler.js:358
-triggerRefValue @ reactivity.esm-bundler.js:974
-(anonymous) @ reactivity.esm-bundler.js:1135
-triggerEffect @ reactivity.esm-bundler.js:373
-triggerEffects @ reactivity.esm-bundler.js:358
-triggerRefValue @ reactivity.esm-bundler.js:974
-set value @ reactivity.esm-bundler.js:1018
-finalizeNavigation @ vue-router.mjs:3355
-(anonymous) @ vue-router.mjs:3220
-Promise.then (async)
-pushWithRedirect @ vue-router.mjs:3187
-push @ vue-router.mjs:3112
-install @ vue-router.mjs:3551
-use @ runtime-core.esm-bundler.js:3752
-(anonymous) @ main.ts:13
-Show 59 more frames
-Show less
-transport.viewmodel.ts:46 Error: SyntaxError: Unexpected end of input (at transport.viewmodel.ts:41:36)
-    at transport.viewmodel.ts:41:36
+export class TransportComponentViewModel {
+  model: TransportComponentModel;
+  constructor(model: TransportComponentModel) {
+    this.model = model;
+    this.readFromQueue();  // Вызов функции при инициализации ViewModel
+  }
 
+  readFromQueue(): void {
+    const username = "your_username";
+    const password = "your_password";
+    
+    fetch('http://rabbitmq.next.local/api/queues/%2F/TmsQueue/get', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Basic ${btoa(`${username}:${password}`)}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "count": 1,
+        "ackmode": "ack_requeue_true",
+        "encoding": "auto",
+        "truncate": 50000
+      })
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to fetch');
+      }
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
 
+  // ... ваш остальной код
+}
 
-
-
-
-
-
-
-
-401 Unauthorized
