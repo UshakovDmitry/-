734f4b420e7d5317c4916bc32f2e10328b2caa33
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-05 11:20:22 +0600

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -1,80 +1,80 @@
-transport.viewmodel.ts:29 
- POST http://rabbitmq.next.local/api/queues/%2F/TmsQueue/get net::ERR_ABORTED 401 (Unauthorized)
-readFromQueue	@	transport.viewmodel.ts:29
-TransportComponentViewModel	@	transport.viewmodel.ts:8
-setup	@	transport.component.vue:53
-Promise.then (async)		
-(anonymous)	@	main.ts:13
+Access to fetch at 'http://rabbitmq.next.local/api/queues/%2F/TmsQueue/get' from origin 'http://127.0.0.1:5173' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
+transport.viewmodel.ts:29     POST http://rabbitmq.next.local/api/queues/%2F/TmsQueue/get net::ERR_FAILED
+readFromQueue @ transport.viewmodel.ts:29
+TransportComponentViewModel @ transport.viewmodel.ts:8
+setup @ transport.component.vue:53
+callWithErrorHandling @ runtime-core.esm-bundler.js:158
+setupStatefulComponent @ runtime-core.esm-bundler.js:7236
+setupComponent @ runtime-core.esm-bundler.js:7197
+mountComponent @ runtime-core.esm-bundler.js:5599
+processComponent @ runtime-core.esm-bundler.js:5565
+patch @ runtime-core.esm-bundler.js:5040
+componentUpdateFn @ runtime-core.esm-bundler.js:5708
+run @ reactivity.esm-bundler.js:178
+instance.update @ runtime-core.esm-bundler.js:5814
+setupRenderEffect @ runtime-core.esm-bundler.js:5822
+mountComponent @ runtime-core.esm-bundler.js:5612
+processComponent @ runtime-core.esm-bundler.js:5565
+patch @ runtime-core.esm-bundler.js:5040
+mountChildren @ runtime-core.esm-bundler.js:5284
+mountElement @ runtime-core.esm-bundler.js:5191
+processElement @ runtime-core.esm-bundler.js:5156
+patch @ runtime-core.esm-bundler.js:5028
+mountChildren @ runtime-core.esm-bundler.js:5284
+mountElement @ runtime-core.esm-bundler.js:5191
+processElement @ runtime-core.esm-bundler.js:5156
+patch @ runtime-core.esm-bundler.js:5028
+mountChildren @ runtime-core.esm-bundler.js:5284
+mountElement @ runtime-core.esm-bundler.js:5191
+processElement @ runtime-core.esm-bundler.js:5156
+patch @ runtime-core.esm-bundler.js:5028
+componentUpdateFn @ runtime-core.esm-bundler.js:5708
+run @ reactivity.esm-bundler.js:178
+instance.update @ runtime-core.esm-bundler.js:5814
+setupRenderEffect @ runtime-core.esm-bundler.js:5822
+mountComponent @ runtime-core.esm-bundler.js:5612
+processComponent @ runtime-core.esm-bundler.js:5565
+patch @ runtime-core.esm-bundler.js:5040
+componentUpdateFn @ runtime-core.esm-bundler.js:5773
+run @ reactivity.esm-bundler.js:178
+instance.update @ runtime-core.esm-bundler.js:5814
+callWithErrorHandling @ runtime-core.esm-bundler.js:158
+flushJobs @ runtime-core.esm-bundler.js:357
+Promise.then (async)
+queueFlush @ runtime-core.esm-bundler.js:270
+queuePostFlushCb @ runtime-core.esm-bundler.js:290
+queueEffectWithSuspense @ runtime-core.esm-bundler.js:1603
+scheduler @ runtime-core.esm-bundler.js:1773
+triggerEffect @ reactivity.esm-bundler.js:373
+triggerEffects @ reactivity.esm-bundler.js:363
+triggerRefValue @ reactivity.esm-bundler.js:974
+(anonymous) @ reactivity.esm-bundler.js:1135
+triggerEffect @ reactivity.esm-bundler.js:373
+triggerEffects @ reactivity.esm-bundler.js:358
+triggerRefValue @ reactivity.esm-bundler.js:974
+(anonymous) @ reactivity.esm-bundler.js:1135
+triggerEffect @ reactivity.esm-bundler.js:373
+triggerEffects @ reactivity.esm-bundler.js:358
+triggerRefValue @ reactivity.esm-bundler.js:974
+set value @ reactivity.esm-bundler.js:1018
+finalizeNavigation @ vue-router.mjs:3355
+(anonymous) @ vue-router.mjs:3220
+Promise.then (async)
+pushWithRedirect @ vue-router.mjs:3187
+push @ vue-router.mjs:3112
+install @ vue-router.mjs:3551
+use @ runtime-core.esm-bundler.js:3752
+(anonymous) @ main.ts:13
 Show 59 more frames
-transport.viewmodel.ts:55 Error: Failed to fetch
-    at transport.viewmodel.ts:48:17
-(anonymous)	@	transport.viewmodel.ts:55
-Promise.catch (async)		
-readFromQueue	@	transport.viewmodel.ts:54
-TransportComponentViewModel	@	transport.viewmodel.ts:8
-setup	@	transport.component.vue:53
-Promise.then (async)		
-(anonymous)	@	main.ts:13
-Show 59 more frames
-
-
-
-
-import { type TransportComponentModel } from './transport.model';
-
-export class TransportComponentViewModel {
-  model: TransportComponentModel;
-
-  constructor(model: TransportComponentModel) {
-    this.model = model;
-    this.readFromQueue();
-  }
-
-  setLoaders(): void {
-    this.model.isTransport = false;
-    this.model.isLoaders = true;
-  }
-
-  setTransport(): void {
-    this.model.isLoaders = false;
-    this.model.isTransport = true;
-  }
-
-  changePage(page: number): void {
-    console.log(page);
-  }
-
-  readFromQueue(): void {
-    const username = 'tms';
-    const password = '26000567855499290979';
-
-    fetch('http://rabbitmq.next.local/api/queues/%2F/TmsQueue/get', {
-      method: 'POST',
-      mode: 'no-cors',
-      headers: {
-        Authorization: `Basic ${btoa(`${username}:${password}`)}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        count: 1,
-        ackmode: 'ack_requeue_true',
-        encoding: 'auto',
-        truncate: 50000,
-      }),
-    })
-      .then(async (response) => {
-        if (response.ok) {
-          console.log(response.json());
-          return await response.json();
-        } else {
-          throw new Error('Failed to fetch');
-        }
-      })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-}
+Show less
+transport.viewmodel.ts:55 TypeError: Failed to fetch
+    at TransportComponentViewModel.readFromQueue (transport.viewmodel.ts:29:5)
+    at new TransportComponentViewModel (transport.viewmodel.ts:8:10)
+    at setup (transport.component.vue:53:3)
+    at callWithErrorHandling (runtime-core.esm-bundler.js:158:18)
+    at setupStatefulComponent (runtime-core.esm-bundler.js:7236:25)
+    at setupComponent (runtime-core.esm-bundler.js:7197:36)
+    at mountComponent (runtime-core.esm-bundler.js:5599:7)
+    at processComponent (runtime-core.esm-bundler.js:5565:9)
+    at patch (runtime-core.esm-bundler.js:5040:11)
+    at ReactiveEffect.componentUpdateFn [as fn] (runtime-core.esm-bundler.js:5708:11)
