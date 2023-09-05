954066401783f6e616a29e772df11144681f05ac
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-05 11:47:08 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -5,3 +5,4 @@ runtime-dom.esm-bundler.js:33 Uncaught (in promise) TypeError: Cannot read prope
     at instance.update (runtime-core.esm-bundler.js:5814:51)
     at callWithErrorHandling (runtime-core.esm-bundler.js:158:32)
     at flushJobs (runtime-core.esm-bundler.js:357:9)
+client.ts:524 WebSocket connection to 'ws://rabbitmq.next.local:15674/ws' failed: 
