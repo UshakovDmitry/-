8cb90ec00633df33366afb47d19bc5f48ed04694
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-05 11:59:39 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,113 @@
-runtime-dom.esm-bundler.js:33 Uncaught (in promise) TypeError: Cannot read properties of null (reading 'parentNode')
-    at parentNode (runtime-dom.esm-bundler.js:33:30)
-    at ReactiveEffect.componentUpdateFn [as fn] (runtime-core.esm-bundler.js:5777:11)
-    at ReactiveEffect.run (reactivity.esm-bundler.js:178:19)
-    at instance.update (runtime-core.esm-bundler.js:5814:51)
-    at callWithErrorHandling (runtime-core.esm-bundler.js:158:32)
-    at flushJobs (runtime-core.esm-bundler.js:357:9)
-client.ts:524 WebSocket connection to 'ws://rabbitmq.next.local:15674/ws' failed: 
+<template>
+  <div class="pagination">
+    <div class="page-btn" @click="goToPrevPage">
+      <IconComponent
+        :сonfig="{
+          name: 'keyboardLeft',
+          color: '#23362D4D',
+          width: 24,
+          height: 24,
+        }"
+      ></IconComponent>
+    </div>
+
+    <button
+      v-for="page in totalPages"
+      :key="page"
+      class="page-btn"
+      @click="goToPage(page)"
+      :class="{ active: currentPage === page }"
+    >
+      {{ page }}
+    </button>
+
+    <div class="page-btn" @click="goToNextPage">
+      <IconComponent
+        :сonfig="{
+          name: 'keyboardRight',
+          color: '#23362D4D',
+          width: 24,
+          height: 24,
+        }"
+      ></IconComponent>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import IconComponent from '../icon/icon.component.vue';
+
+const props = defineProps<{
+  totalPages: number;
+  initialPage: number;
+}>();
+
+const emit = defineEmits<{
+  (e: 'page-change', value: number): void;
+}>();
+
+const currentPage = ref(props.initialPage);
+
+const goToPage = (page: number) => {
+  currentPage.value = page;
+  emit('page-change', page);
+};
+
+const goToPrevPage = () => {
+  if (currentPage.value > 1) {
+    goToPage(currentPage.value - 1);
+  }
+};
+
+const goToNextPage = () => {
+  if (currentPage.value < props.totalPages) {
+    goToPage(currentPage.value + 1);
+  }
+};
+</script>
+
+<style scoped>
+.pagination {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+}
+.page-btn {
+  display: flex;
+  width: 34px;
+  height: 34px;
+  padding: 4px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 80px;
+  outline: none;
+  border: none;
+  color: var(--text-dark, #23362d);
+  text-align: center;
+  font-family: Rubik;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 12px; /* 120% */
+  letter-spacing: 0.5px;
+  cursor: pointer;
+  /* border: 1px solid #813909; */
+}
+
+button.active {
+  border-radius: 80px;
+  background: var(--overlay-activated, rgba(0, 161, 83, 0.12));
+  color: var(--primary-light-mode-dark, #006f39);
+  text-align: center;
+
+}
+
+button:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
+}
+</style>
+
