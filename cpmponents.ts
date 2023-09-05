c53f668dac182679e8832a603216a2ade7b351ec
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-05 16:52:32 +0600

    Update cpmponents.ts

--- cpmponents.ts
+++ cpmponents.ts
@@ -1,74 +1,16 @@
-<template>
-  <div class="navbar">
-    <router-link to="/dashboard">
-      <IconComponent
-        :сonfig="{
-          name: 'dashboardIcon',
-          color: isActive ? '#01A254' : '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      >
-      </IconComponent>
-    </router-link>
+  selectCity(city: string): void {
+    console.log(city);
+    this.sortTable(city);
+  }
+  sortTable(city: string): void {
+    this.model.transport.sort((a, b) => {
+      if (a[city] > b[city]) {
+        return 1;
+      }
+      if (a[city] < b[city]) {
+        return -1;
+      }
+      return 0;
+    });
+  }
 
-    <router-link to="/map">
-      <IconComponent
-        :сonfig="{
-          name: 'mapIcon',
-          color: isActive ? '#01A254' : '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      >
-      </IconComponent>
-    </router-link>
-
-    <router-link to="/applications">
-      <IconComponent
-        :сonfig="{
-          name: 'assignmentIcon',
-          color: isActive ? '#01A254' : '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      >
-      </IconComponent>
-    </router-link>
-
-    <router-link to="/delivery">
-      <IconComponent
-        :сonfig="{
-          name: 'peopleIcon',
-          color: isActive ? '#01A254' : '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      >
-      </IconComponent>
-    </router-link>
-
-    <router-link to="/transport">
-      <IconComponent
-        :сonfig="{
-          name: 'carIcon',
-          color: isActive ? '#01A254' : '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      >
-      </IconComponent>
-    </router-link>
-  </div>
-</template>
-
-<script setup lang="ts">
-import IconComponent from '../components/global/icon/icon.component.vue';
-// import { computed } from 'vue';
-import { useRoute } from 'vue-router';
-
-const route = useRoute();
-const isActive = (path: string): boolean => {
-  return route.path === path;
-};
-</script>
