43da4d1123a288a612f7e15c783eec940ae6ecd2
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-06 14:42:39 +0600

    Update cpmponents.ts

--- cpmponents.ts
+++ cpmponents.ts
@@ -1,3 +1,32 @@
+<template>
+  <div class="dropdown">
+    <div class="dropdown__header" >
+      <div class="dropdown__btn" data-test="dropdown-btn" @click="toggle">
+        <IconComponent
+          :сonfig="{
+            name: 'location',
+            color: '#23362D4D',
+            width: 24,
+            height: 24,
+          }"
+        ></IconComponent>
+        <p>
+          {{ currentValue.length ? currentValue : 'Выберите город' }}
+        </p>
+        <div>
+          <IconComponent
+            :сonfig="{
+              name: 'expand_more',
+              color: '#23362D4D',
+              width: 24,
+              height: 24,
+            }"
+          ></IconComponent>
+        </div>
+      </div>
+    </div>
+    <!-- Анимация при открытии/закрытии -->
+    <transition name="fade">
       <!-- Отображаем список городов, если переменная isOpened истинна -->
       <div v-if="isOpened" data-test="dropdown-body" class="dropdown__body" :style="{ width: width + 'px' }">
         <ul class="dropdown__list">
@@ -14,4 +43,6 @@
           </li>
         </ul>
       </div>
-
+    </transition>
+  </div>
+</template>
