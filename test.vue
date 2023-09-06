bc35e9cf20004e5bac342a6fdba3007e9c25bcd5
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-06 10:56:21 +0600

    Update test.vue

--- test.vue
+++ test.vue
@@ -1,51 +1,22 @@
 <template>
-  <section class="auth">
-    <div class="auth__content">
-      <img :src="logo" alt="logo" />
-     
-      <ButtonComponent
-        :config="{
-          value: 'Войти',
-          type: 'filled',
-          color: 'green',
-          border: 'large',
-          loading: false,
-          disabled: false,
-          width: '100%',
-
-        }"
-        @input="console.log('click')"
-      ></ButtonComponent>
+  <div>
+    <HeaderComponent v-if="!isAuthPage" />
+    <div class="main-layout">
+      <NavbarComponent v-if="!isAuthPage" />
+      <div class="content">
+        <router-view></router-view>
+      </div>
     </div>
-  </section>
+  </div>
 </template>
+
 <script setup lang="ts">
-import logo from '../../public/icons/logo.svg';
-import ButtonComponent from '../../components/global/button/button.vue';
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+import HeaderComponent from '../components/HeaderComponent.vue';
+import NavbarComponent from '../components/NavbarComponent.vue';
 
-
+const route = useRoute();
+const isAuthPage = ref(route.path === '/auth');
 </script>
-<style scoped lang="scss">
-.auth {
-  max-width: 100vw;
-  height: 100vh;
-  background: linear-gradient(108deg, #01a254 0%, #50d17f 100%);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-left: -72px;
-  margin-top: -107px;
-}
-.auth__content {
-  display: flex;
-  width: 420px;
-  padding: 36px;
-  flex-direction: column;
-  align-items: center;
-  gap: 32px;
-  flex-shrink: 0;
-  border-radius: 16px;
-  background: #fff;
-}
-</style>
 
