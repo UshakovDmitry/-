e05817f84d8fdf8668333859e524b67197c67d07
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-06 15:30:41 +0600

    Update toggle.vue

--- toggle.vue
+++ toggle.vue
@@ -1,168 +1,45 @@
-<template>
-  <div class="pagination">
-    <div
-      class="first-page-btn"
-      data-test="first-page-btn"
-      @click="goToFirstPage"
-    >
-      <IconComponent
-        :сonfig="{
-          name: 'doubleArrowRight',
-          color: '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      ></IconComponent>
-    </div>
-    <div class="page-btn" data-test="prev-page-btn" @click="goToPrevPage">
-      <IconComponent
-        :сonfig="{
-          name: 'keyboardLeft',
-          color: '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      ></IconComponent>
-    </div>
-
-    <button
-      v-for="page in totalPages"
-      :key="page"
-      class="page-btn"
-      data-test="page-btn"
-      @click="goToPage(page)"
-      :class="{ active: currentPage === page }"
-    >
-      {{ page }}
-    </button>
-
-    <div class="page-btn" data-test="next-page-btn" @click="goToNextPage">
-      <IconComponent
-        :сonfig="{
-          name: 'keyboardRight',
-          color: '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      ></IconComponent>
-    </div>
-    <div class="last-page-btn" data-test="last-page-btn" @click="goToLastPage">
-      <IconComponent
-        :сonfig="{
-          name: 'doubleArrowRight',
-          color: '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      ></IconComponent>
-    </div>
-  </div>
-</template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-import IconComponent from '../icon/icon.component.vue';
-
-const props = defineProps<{
-  totalPages: number;
-  initialPage: number;
-}>();
-
-const emit = defineEmits<{
-  (e: 'page-change', value: number): void;
-}>();
-
-const currentPage = ref(props.initialPage);
-
-const goToPage = (page: number) => {
-  currentPage.value = page;
-  emit('page-change', page);
-};
-
-const goToPrevPage = () => {
-  if (currentPage.value > 1) {
-    goToPage(currentPage.value - 1);
-  }
-};
-
-const goToNextPage = () => {
-  if (currentPage.value < props.totalPages) {
-    goToPage(currentPage.value + 1);
-  }
-};
-const goToFirstPage = () => {
-  goToPage(1);
-};
-
-const goToLastPage = () => {
-  goToPage(props.totalPages);
-};
-</script>
-
-<style scoped>
-.pagination {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 6px;
-}
-.first-page-btn {
-  display: flex;
-  width: 34px;
-  height: 34px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 80px;
-  padding-top: 2px;
-  outline: none;
-  border: none;
-  box-sizing: border-box;
-  rotate: 180deg;
-  color: var(--text-dark, #23362d);
-  cursor: pointer;
-  /* border: 1px solid #813909; */
-}
-.last-page-btn {
-  display: flex;
-  width: 34px;
-  height: 34px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 80px;
-  padding-top: 5px;
-  box-sizing: border-box;
-  outline: none;
-  border: none;
-  color: var(--text-dark, #23362d);
-  cursor: pointer;
-  /* border: 1px solid #813909; */
-}
-.page-btn {
-  display: flex;
-  width: 34px;
-  height: 34px;
-  /* padding: 4px; */
-  /* flex-direction: column; */
-  justify-content: center;
-  align-items: center;
-  border-radius: 80px;
-  outline: none;
-  border: none;
-  color: var(--text-dark, #23362d);
-  cursor: pointer;
-  /* border: 1px solid #813909; */
-}
-
-button.active {
-  border-radius: 80px;
-  background: var(--overlay-activated, rgba(0, 161, 83, 0.12));
-  color: var(--primary-light-mode-dark, #006f39);
-  text-align: center;
-}
-
-button:disabled {
-  background-color: #ccc;
-  cursor: not-allowed;
-}
-</style>
-
+table-switch.component.vue:13 [Vue warn]: Unhandled error during execution of component event handler 
+  at <TableSwitch.component isTransport=true isLoaders=false onSetTransport=fn<setTransport>  ... > 
+  at <Transport.component onVnodeUnmounted=fn<onVnodeUnmounted> ref=Ref< Proxy(Object) {__v_skip: true} > > 
+  at <RouterView> 
+  at <Dafault onVnodeUnmounted=fn<onVnodeUnmounted> ref=Ref< Proxy(Object) {__v_skip: true} > > 
+  at <RouterView> 
+  at <App>
+warn2 @ runtime-core.esm-bundler.js:41
+logError @ runtime-core.esm-bundler.js:216
+handleError @ runtime-core.esm-bundler.js:208
+callWithErrorHandling @ runtime-core.esm-bundler.js:160
+callWithAsyncErrorHandling @ runtime-core.esm-bundler.js:166
+emit @ runtime-core.esm-bundler.js:664
+(anonymous) @ runtime-core.esm-bundler.js:7422
+_createElementVNode.onClick._cache.<computed>._cache.<computed> @ table-switch.component.vue:13
+callWithErrorHandling @ runtime-core.esm-bundler.js:158
+callWithAsyncErrorHandling @ runtime-core.esm-bundler.js:166
+invoker @ runtime-dom.esm-bundler.js:278
+Show 10 more frames
+Show less
+runtime-core.esm-bundler.js:41 [Vue warn]: Unhandled error during execution of native event handler 
+  at <TableSwitch.component isTransport=true isLoaders=false onSetTransport=fn<setTransport>  ... > 
+  at <Transport.component onVnodeUnmounted=fn<onVnodeUnmounted> ref=Ref< Proxy(Object) {__v_skip: true} > > 
+  at <RouterView> 
+  at <Dafault onVnodeUnmounted=fn<onVnodeUnmounted> ref=Ref< Proxy(Object) {__v_skip: true} > > 
+  at <RouterView> 
+  at <App>
+warn2 @ runtime-core.esm-bundler.js:41
+logError @ runtime-core.esm-bundler.js:216
+handleError @ runtime-core.esm-bundler.js:208
+callWithErrorHandling @ runtime-core.esm-bundler.js:160
+callWithAsyncErrorHandling @ runtime-core.esm-bundler.js:166
+invoker @ runtime-dom.esm-bundler.js:278
+Show 6 more frames
+Show less
+runtime-core.esm-bundler.js:221 Uncaught TypeError: Cannot read properties of undefined (reading 'model')
+    at setLoaders (transport.viewmodel.ts:18:10)
+    at callWithErrorHandling (runtime-core.esm-bundler.js:158:18)
+    at callWithAsyncErrorHandling (runtime-core.esm-bundler.js:166:17)
+    at emit (runtime-core.esm-bundler.js:664:5)
+    at Proxy.<anonymous> (runtime-core.esm-bundler.js:7422:45)
+    at _createElementVNode.onClick._cache.<computed>._cache.<computed> (table-switch.component.vue:13:15)
+    at callWithErrorHandling (runtime-core.esm-bundler.js:158:18)
+    at callWithAsyncErrorHandling (runtime-core.esm-bundler.js:166:17)
+    at HTMLDivElement.invoker (runtime-dom.esm-bundler.js:278:5)
