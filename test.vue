b88e8dfe61371505820c0013220f4de88333c540
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-06 11:24:26 +0600

    Update test.vue

--- test.vue
+++ test.vue
@@ -1,5 +1,14 @@
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
+      <VueDatePicker
+        v-model="date"
+        locale="ru"
+        range
+        :placeholder="'Выберите дату'"
+        class="date_picker"
+      ></VueDatePicker>
+
+
+
+      .date_picker {
+  --dp-cell-border-radius: 16px; /*Specific border radius for the calendar cell*/
+  width: 200px;
 }
