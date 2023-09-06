e27daa31be4757060c405e72af24c0f4071acdc1
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-06 15:52:08 +0600

    Update test.vue

--- test.vue
+++ test.vue
@@ -1,40 +1,44 @@
-    <div class="wrapper-toggle-btns">
-      <div
-        class="wrapper-toggle-btns-item"
-        :class="{ isActiveTab: model.isTransport === true }"
-        @click="viewModel.setTransport()"
-      >
-        Транспорт
-      </div>
-      <div
-        class="wrapper-toggle-btns-item"
-        :class="{ isActiveTab: model.isLoaders === true }"
-        @click="viewModel.setLoaders()"
-      >
-        Грузчики
-      </div>
-    </div>
+describe('Toggle Buttons E2E tests', () => {
+  beforeEach(() => {
+    cy.visit('/your-page-url'); // Замените на URL страницы, где размещён ваш переключатель
+  });
+
+  it('should activate the Transport button when clicked', () => {
+    cy.get('[data-test="transport-btn"]').click().should('have.class', 'isActiveTab');
+    cy.get('[data-test="loaders-btn"]').should('not.have.class', 'isActiveTab');
+  });
+
+  it('should activate the Loaders button when clicked', () => {
+    cy.get('[data-test="loaders-btn"]').click().should('have.class', 'isActiveTab');
+    cy.get('[data-test="transport-btn"]').should('not.have.class', 'isActiveTab');
+  });
+
+  it('should toggle between Transport and Loaders when clicked', () => {
+    cy.get('[data-test="transport-btn"]').click().should('have.class', 'isActiveTab');
+    cy.get('[data-test="loaders-btn"]').click().should('have.class', 'isActiveTab');
+    cy.get('[data-test="transport-btn"]').should('not.have.class', 'isActiveTab');
+  });
+});
 
 
 
 
+<div class="wrapper-toggle-btns">
+  <div
+    class="wrapper-toggle-btns-item"
+    data-test="transport-btn"
+    :class="{ isActiveTab: model.isTransport === true }"
+    @click="viewModel.setTransport()"
+  >
+    Транспорт
+  </div>
+  <div
+    class="wrapper-toggle-btns-item"
+    data-test="loaders-btn"
+    :class="{ isActiveTab: model.isLoaders === true }"
+    @click="viewModel.setLoaders()"
+  >
+    Грузчики
+  </div>
+</div>
 
-.wrapper-toggle-btns {
-  width: 394px;
-  height: 44px;
-  display: flex;
-  flex-direction: row;
-  margin-top: 20px;
-}
-.wrapper-toggle-btns-item {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-}
-.isActiveTab {
-  color: var(--primary-light-mode-main, #00a153);
-  border-bottom: 3px solid var(--primary-light-mode-main, #00a153);
-}
