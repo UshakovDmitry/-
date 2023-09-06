cd00e6c6684e51f52384624d90919d8a6e67163f
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-06 17:22:07 +0600

    Update test.vue

--- test.vue
+++ test.vue
@@ -1,3 +1,4 @@
+import * as XLSX from 'xlsx';
 import { type TransportComponentModel } from './transport.model';
 
 export class TransportComponentViewModel {
@@ -29,5 +30,35 @@ export class TransportComponentViewModel {
   setTransport(): void {
     this.model.isLoaders = false;
     this.model.isTransport = true;
+  }
+
+  downloadLoadersAsXLSX(): void {
+    const headersLoaders = [
+      'ФИО',
+      '№ Склада',
+      'График работы',
+      '№ транспорта',
+      'Активность',
+    ];
+
+    const loaders = this.model.loaders;  // Предположим, что данные о загрузчиках хранятся в this.model.loaders
+
+    const data = [headersLoaders];
+
+    loaders.forEach(loader => {
+      const row = [];
+      row.push(loader.fullName);
+      row.push(loader.warehouseNumber);
+      row.push(loader.schedule);
+      row.push(loader.transportNumber);
+      row.push(loader.isActive ? 'Да' : 'Нет');
+      data.push(row);
+    });
+
+    const ws = XLSX.utils.aoa_to_sheet(data);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    XLSX.writeFile(wb, 'Loaders.xlsx');
   }
 }
