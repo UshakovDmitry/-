698f67da62c68c122c2fb1e6d61055002c044fb1
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-06 12:09:03 +0600

    Update cpmponents.ts

--- cpmponents.ts
+++ cpmponents.ts
@@ -1,10 +1,38 @@
-.date_picker {
- --dp-border-radius: 44px;
- /* --dp-cell-border-radius: 4px;  */
- width: 200px;
-}
+CypressError
+cy.visit() failed trying to load:
 
-.date_picker .dp__outer_menu_wrap .dp--menu-wrapper {
-  --dp-border-radius: 4px;
-  --dp-cell-border-radius: 4px;
-}
+
+
+http://127.0.0.1:5173/
+
+
+
+We attempted to make an http request to this URL but the request failed without a response.
+
+
+
+We received this error at the network level:
+
+
+
+  > Error: connect ECONNREFUSED 127.0.0.1:5173
+
+
+
+Common situations why this would fail:
+
+  - you don't have internet access
+
+  - you forgot to run / boot your web server
+
+  - your web server isn't accessible
+
+  - you have weird network configuration settings on your computer
+alser.dispatcherworkplaceui/cypress/e2e/project.cy.ts:4:1
+  2 | describe('template spec', () => {
+  3 |     it('passes', () => {
+> 4 |         cy.visit('http://127.0.0.1:5173');
+    | ^
+  5 |     });
+  6 | });
+  7 | //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJvamVjdC5jeS5qcyIsInNvdXJjZ
