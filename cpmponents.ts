0b5e5b0ac3b1e72705dc4169f3fe450df8061975
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-06 14:44:22 +0600

    Update cpmponents.ts

--- cpmponents.ts
+++ cpmponents.ts
@@ -1,48 +1,30 @@
-<template>
-  <div class="dropdown">
-    <div class="dropdown__header" >
-      <div class="dropdown__btn" data-test="dropdown-btn" @click="toggle">
-        <IconComponent
-          :сonfig="{
-            name: 'location',
-            color: '#23362D4D',
-            width: 24,
-            height: 24,
-          }"
-        ></IconComponent>
-        <p>
-          {{ currentValue.length ? currentValue : 'Выберите город' }}
-        </p>
-        <div>
-          <IconComponent
-            :сonfig="{
-              name: 'expand_more',
-              color: '#23362D4D',
-              width: 24,
-              height: 24,
-            }"
-          ></IconComponent>
-        </div>
-      </div>
-    </div>
-    <!-- Анимация при открытии/закрытии -->
-    <transition name="fade">
-      <!-- Отображаем список городов, если переменная isOpened истинна -->
-      <div v-if="isOpened" data-test="dropdown-body" class="dropdown__body" :style="{ width: width + 'px' }">
-        <ul class="dropdown__list">
-          <!-- Проходим по каждому городу в списке и отображаем его -->
-          <li
-            v-for="item in items"
-            :key="item"
-            class="dropdown__item"
-            @click="selectItem(item)"
-          >
-            <div class="dropdown__item__name">
-              {{ item }}
-            </div>
-          </li>
-        </ul>
-      </div>
-    </transition>
-  </div>
-</template>
+describe('template spec', () => {
+  // TRANSPORT
+  beforeEach(() => {
+    cy.visit('/transport');
+  });
+  it('should display a list of cities', () => {
+    // Выбор города
+    cy.get('[data-test="dropdown-body"]').should('not.exist');
+    cy.get('[data-test="dropdown-btn"]').click();
+    cy.get('[data-test="dropdown-body"]').should('be.visible');
+  });
+// Поиск по номеру ППО
+  it('should display text', () => {
+    const numberPPO = 12345567;
+    cy.get('[data-test="search-input-PPO"]').should('be.visible');
+    cy.get('[data-test="search-input-PPO"]')
+      .should('be.visible')
+      .type(`${numberPPO}{enter}`);
+  });
+  // Календарь
+  // it('should display a calendar', () => {
+  //   cy.get('[data-test="calendar-btn"]').click();
+  //   cy.get('[data-test="calendar-body"]').should('be.visible');
+  // });
+  
+  // Скачать файл
+  it('should download file', () => {
+    cy.get('[data-test="download-btn"]').click({force: true});   
+  });
+});
