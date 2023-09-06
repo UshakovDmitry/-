d60e3f5a281511cba624fa6b77ca4ea63e1672a1
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-06 15:36:11 +0600

    Update test.vue

--- test.vue
+++ test.vue
@@ -1,14 +1,641 @@
-      <VueDatePicker
-        v-model="date"
-        locale="ru"
-        range
-        :placeholder="'Выберите дату'"
-        class="date_picker"
-      ></VueDatePicker>
+import { type ITransport } from '../../domain/interfaces/transport.interface';
+import { type ILoader } from '../../domain/interfaces/loader.interface';
 
-
-
-      .date_picker {
-  --dp-cell-border-radius: 16px; /*Specific border radius for the calendar cell*/
-  width: 200px;
+interface ITransportComponentModel {
+  isLoaders: boolean;
+  isTransport: boolean;
+  transport: ITransport[];
+  loaders: ILoader[];
+  headersTransport: string[];
+  headersLoaders: string[];
+  configTransport: any;
+  configLoaders: any;
 }
+
+export class TransportComponentModel implements ITransportComponentModel {
+  isLoaders: boolean;
+  isTransport: boolean;
+  transport: ITransport[];
+  headersTransport: string[];
+  headersLoaders: string[];
+  loaders: ILoader[];
+  configTransport: any;
+  configLoaders: any;
+
+  constructor() {
+    this.isLoaders = false;
+    this.isTransport = true;
+    this.transport = [
+      {
+        model: 'Mercedes-Benz',
+        number: 'A 123 AA 77',
+        type: 'Газель',
+        volume: '4.5',
+        loadCapacity: '1.5',
+        city: 'Москва',
+        isActive: true,
+        schedule: 'Пн-Пт 9:00-18:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Ford Transit',
+        number: 'B 234 BB 88',
+        type: 'Грузовик',
+        volume: '6.0',
+        loadCapacity: '2.5',
+        city: 'Санкт-Петербург',
+        isActive: false,
+        schedule: 'Пн-Пт 8:00-20:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Volkswagen Crafter',
+        number: 'C 345 CC 99',
+        type: 'Минивэн',
+        volume: '5.0',
+        loadCapacity: '1.2',
+        city: 'Новосибирск',
+        isActive: true,
+        schedule: 'Сб-Вс 10:00-16:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Iveco Daily',
+        number: 'D 456 DD 10',
+        type: 'Грузовик',
+        volume: '7.0',
+        loadCapacity: '3.0',
+        city: 'Екатеринбург',
+        isActive: false,
+        schedule: 'Пн-Вс 9:00-21:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Renault Master',
+        number: 'F 678 FF 12',
+        type: 'Грузовик',
+        volume: '6.5',
+        loadCapacity: '2.8',
+        city: 'Нижний Новгород',
+        isActive: true,
+        schedule: 'Пн-Пт 10:00-19:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Peugeot Boxer',
+        number: 'G 789 GG 13',
+        type: 'Минивэн',
+        volume: '4.8',
+        loadCapacity: '1.8',
+        city: 'Самара',
+        isActive: false,
+        schedule: 'Сб-Вс 11:00-17:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Fiat Ducato',
+        number: 'H 890 HH 14',
+        type: 'Грузовик',
+        volume: '5.7',
+        loadCapacity: '1.9',
+        city: 'Омск',
+        isActive: true,
+        schedule: 'Пн-Пт 12:00-20:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Nissan NV400',
+        number: 'I 901 II 15',
+        type: 'Минивэн',
+        volume: '5.2',
+        loadCapacity: '1.7',
+        city: 'Челябинск',
+        isActive: false,
+        schedule: 'Пн-Пт 13:00-21:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Citroën Jumper',
+        number: 'J 012 JJ 16',
+        type: 'Грузовик',
+        volume: '6.3',
+        loadCapacity: '2.6',
+        city: 'Ростов-на-Дону',
+        isActive: true,
+        schedule: 'Сб-Вс 9:00-15:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Mercedes-Benz Sprinter',
+        number: 'K 123 KK 17',
+        type: 'Минивэн',
+        volume: '4.9',
+        loadCapacity: '1.4',
+        city: 'Уфа',
+        isActive: true,
+        schedule: 'Пн-Пт 14:00-22:00',
+        hasDriver: true,
+      },
+      {
+        model: 'MAN TGE',
+        number: 'E 567 EE 11',
+        type: 'Минивэн',
+        volume: '5.5',
+        loadCapacity: '2.0',
+        city: 'Казань',
+        isActive: true,
+        schedule: 'Пн-Пт 7:00-15:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Renault Master',
+        number: 'F 678 FF 12',
+        type: 'Грузовик',
+        volume: '6.5',
+        loadCapacity: '2.8',
+        city: 'Нижний Новгород',
+        isActive: true,
+        schedule: 'Пн-Пт 10:00-19:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Peugeot Boxer',
+        number: 'G 789 GG 13',
+        type: 'Минивэн',
+        volume: '4.8',
+        loadCapacity: '1.8',
+        city: 'Самара',
+        isActive: false,
+        schedule: 'Сб-Вс 11:00-17:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Fiat Ducato',
+        number: 'H 890 HH 14',
+        type: 'Грузовик',
+        volume: '5.7',
+        loadCapacity: '1.9',
+        city: 'Омск',
+        isActive: true,
+        schedule: 'Пн-Пт 12:00-20:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Nissan NV400',
+        number: 'I 901 II 15',
+        type: 'Минивэн',
+        volume: '5.2',
+        loadCapacity: '1.7',
+        city: 'Челябинск',
+        isActive: false,
+        schedule: 'Пн-Пт 13:00-21:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Citroën Jumper',
+        number: 'J 012 JJ 16',
+        type: 'Грузовик',
+        volume: '6.3',
+        loadCapacity: '2.6',
+        city: 'Ростов-на-Дону',
+        isActive: true,
+        schedule: 'Сб-Вс 9:00-15:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Mercedes-Benz Sprinter',
+        number: 'K 123 KK 17',
+        type: 'Минивэн',
+        volume: '4.9',
+        loadCapacity: '1.4',
+        city: 'Уфа',
+        isActive: true,
+        schedule: 'Пн-Пт 14:00-22:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Vauxhall Movano',
+        number: 'L 234 LL 18',
+        type: 'Грузовик',
+        volume: '7.2',
+        loadCapacity: '3.1',
+        city: 'Волгоград',
+        isActive: false,
+        schedule: 'Пн-Пт 15:00-23:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Iveco Daily',
+        number: 'D 456 DD 10',
+        type: 'Грузовик',
+        volume: '7.0',
+        loadCapacity: '3.0',
+        city: 'Екатеринбург',
+        isActive: false,
+        schedule: 'Пн-Вс 9:00-21:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Renault Master',
+        number: 'F 678 FF 12',
+        type: 'Грузовик',
+        volume: '6.5',
+        loadCapacity: '2.8',
+        city: 'Нижний Новгород',
+        isActive: true,
+        schedule: 'Пн-Пт 10:00-19:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Peugeot Boxer',
+        number: 'G 789 GG 13',
+        type: 'Минивэн',
+        volume: '4.8',
+        loadCapacity: '1.8',
+        city: 'Самара',
+        isActive: false,
+        schedule: 'Сб-Вс 11:00-17:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Fiat Ducato',
+        number: 'H 890 HH 14',
+        type: 'Грузовик',
+        volume: '5.7',
+        loadCapacity: '1.9',
+        city: 'Омск',
+        isActive: true,
+        schedule: 'Пн-Пт 12:00-20:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Nissan NV400',
+        number: 'I 901 II 15',
+        type: 'Минивэн',
+        volume: '5.2',
+        loadCapacity: '1.7',
+        city: 'Челябинск',
+        isActive: false,
+        schedule: 'Пн-Пт 13:00-21:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Citroën Jumper',
+        number: 'J 012 JJ 16',
+        type: 'Грузовик',
+        volume: '6.3',
+        loadCapacity: '2.6',
+        city: 'Ростов-на-Дону',
+        isActive: true,
+        schedule: 'Сб-Вс 9:00-15:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Mercedes-Benz Sprinter',
+        number: 'K 123 KK 17',
+        type: 'Минивэн',
+        volume: '4.9',
+        loadCapacity: '1.4',
+        city: 'Уфа',
+        isActive: true,
+        schedule: 'Пн-Пт 14:00-22:00',
+        hasDriver: true,
+      },
+      {
+        model: 'MAN TGE',
+        number: 'E 567 EE 11',
+        type: 'Минивэн',
+        volume: '5.5',
+        loadCapacity: '2.0',
+        city: 'Казань',
+        isActive: true,
+        schedule: 'Пн-Пт 7:00-15:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Renault Master',
+        number: 'F 678 FF 12',
+        type: 'Грузовик',
+        volume: '6.5',
+        loadCapacity: '2.8',
+        city: 'Нижний Новгород',
+        isActive: true,
+        schedule: 'Пн-Пт 10:00-19:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Peugeot Boxer',
+        number: 'G 789 GG 13',
+        type: 'Минивэн',
+        volume: '4.8',
+        loadCapacity: '1.8',
+        city: 'Самара',
+        isActive: false,
+        schedule: 'Сб-Вс 11:00-17:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Fiat Ducato',
+        number: 'H 890 HH 14',
+        type: 'Грузовик',
+        volume: '5.7',
+        loadCapacity: '1.9',
+        city: 'Омск',
+        isActive: true,
+        schedule: 'Пн-Пт 12:00-20:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Nissan NV400',
+        number: 'I 901 II 15',
+        type: 'Минивэн',
+        volume: '5.2',
+        loadCapacity: '1.7',
+        city: 'Челябинск',
+        isActive: false,
+        schedule: 'Пн-Пт 13:00-21:00',
+        hasDriver: true,
+      },
+      {
+        model: 'Citroën Jumper',
+        number: 'J 012 JJ 16',
+        type: 'Грузовик',
+        volume: '6.3',
+        loadCapacity: '2.6',
+        city: 'Ростов-на-Дону',
+        isActive: true,
+        schedule: 'Сб-Вс 9:00-15:00',
+        hasDriver: false,
+      },
+      {
+        model: 'Mercedes-Benz Sprinter',
+        number: 'K 123 KK 17',
+        type: 'Минивэн',
+        volume: '4.9',
+        loadCapacity: '1.4',
+        city: 'Уфа',
+        isActive: true,
+        schedule: 'Пн-Пт 14:00-22:00',
+        hasDriver: true,
+      },
+    ];
+    this.loaders = [
+      {
+        fullName: 'Иванов Иван Иванович',
+        warehouseNumber: '1',
+        schedule: 'Пн-Пт 9:00-18:00',
+        transportNumber: 'A 123 AA 77',
+        isActive: true,
+      },
+      {
+        fullName: 'Кукин Иван Иванович',
+        warehouseNumber: '1',
+        schedule: 'Пн-Пт 9:00-18:00',
+        transportNumber: 'A 123 AA 77',
+        isActive: true,
+      },
+      {
+        fullName: 'Сидоренко Иван Иванович',
+        warehouseNumber: '1',
+        schedule: 'Пн-Пт 9:00-18:00',
+        transportNumber: 'A 123 AA 77',
+        isActive: true,
+      },
+      {
+        fullName: 'Капалкин Иван Иванович',
+        warehouseNumber: '1',
+        schedule: 'Пн-Пт 9:00-18:00',
+        transportNumber: 'A 123 AA 77',
+        isActive: true,
+      },
+      {
+        fullName: 'Шарапов Иван Иванович',
+        warehouseNumber: '1',
+        schedule: 'Пн-Пт 9:00-18:00',
+        transportNumber: 'A 123 AA 77',
+        isActive: true,
+      },
+      {
+        fullName: 'Питьков Иван Иванович',
+        warehouseNumber: '1',
+        schedule: 'Пн-Пт 9:00-18:00',
+        transportNumber: 'A 123 AA 77',
+        isActive: true,
+      },
+      {
+        fullName: 'Иванов Иван Иванович',
+        warehouseNumber: '1',
+        schedule: 'Пн-Пт 9:00-18:00',
+        transportNumber: 'A 123 AA 77',
+        isActive: true,
+      },
+      {
+        fullName: 'Кукин Иван Иванович',
+        warehouseNumber: '1',
+        schedule: 'Пн-Пт 9:00-18:00',
+        transportNumber: 'A 123 AA 77',
+        isActive: true,
+      },
+      {
+        fullName: 'Сидоренко Иван Иванович',
+        warehouseNumber: '1',
+        schedule: 'Пн-Пт 9:00-18:00',
+        transportNumber: 'A 123 AA 77',
+        isActive: true,
+      },
+      {
+        fullName: 'Капалкин Иван Иванович',
+        warehouseNumber: '1',
+        schedule: 'Пн-Пт 9:00-18:00',
+        transportNumber: 'A 123 AA 77',
+        isActive: true,
+      },
+      {
+        fullName: 'Шарапов Иван Иванович',
+        warehouseNumber: '1',
+        schedule: 'Пн-Пт 9:00-18:00',
+        transportNumber: 'A 123 AA 77',
+        isActive: true,
+      },
+      {
+        fullName: 'Питьков Иван Иванович',
+        warehouseNumber: '1',
+        schedule: 'Пн-Пт 9:00-18:00',
+        transportNumber: 'A 123 AA 77',
+        isActive: true,
+      },
+    ];
+
+    this.headersTransport = [
+      'Транспорт',
+      'Номер',
+      'Тип',
+      'Объем',
+      'Грузоподъемность',
+      'Город',
+      'Грузчик к транспорту',
+      'График работы',
+      'Активность',
+    ];
+    this.headersLoaders = [
+      'ФИО',
+      '№ Склада',
+      'График работы',
+      '№ транспорта',
+      'Активность',
+    ];
+    this.configTransport = {
+      headers: [
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 2,
+          },
+        },
+        {
+          config: {
+            type: 3,
+          },
+        },
+        {
+          config: {
+            type: 2,
+          },
+        },
+      ],
+      rows: [
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 2,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 2,
+          },
+        },
+      ],
+    };
+    this.configLoaders = {
+      headers: [
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 3,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 2,
+          },
+        },
+      ],
+      rows: [
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 1,
+          },
+        },
+        {
+          config: {
+            type: 2,
+          },
+        },
+      ],
+    };
+  }
+}
+
