b7bf24ea7dc3ca955ff60e4ffea438090da26d5b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-06 17:20:33 +0600

    Update test.vue

--- test.vue
+++ test.vue
@@ -1,8 +1,7 @@
-import { TransportComponentModel } from './transport.model'; // Предполагается, что вы импортируете вашу модель
+import { type TransportComponentModel } from './transport.model';
 
 export class TransportComponentViewModel {
   model: TransportComponentModel;
-  isSortedInAscendingOrder: boolean = true; // Новое свойство для отслеживания порядка сортировки
 
   constructor(model: TransportComponentModel) {
     this.model = model;
@@ -13,6 +12,10 @@ export class TransportComponentViewModel {
   }
 
   filterTableByCity(city: string): void {
+    if (city === 'Все города') {
+      this.model.filteredTransport = this.model.transport;
+      return;
+    }
     this.model.filteredTransport = this.model.transport.filter(
       (item) => item.city === city,
     );
@@ -27,40 +30,4 @@ export class TransportComponentViewModel {
     this.model.isLoaders = false;
     this.model.isTransport = true;
   }
-
-  changePage(page: number): void {
-    console.log(page);
-  }
-
-  readFromQueue(): void {
-    // ... (Как в вашем примере)
-  }
-
-  private getCurrentDayAndTimeInKZ() {
-    const dateInKZ = new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Almaty" }));
-    const currentDay = dateInKZ.getDay();
-    const currentTime = dateInKZ.getHours() * 60 + dateInKZ.getMinutes();
-    return { currentDay, currentTime };
-  }
-
-  private isOperatingTodayAndNow(schedule: string, currentDay: number, currentTime: number): boolean {
-    // Здесь будет ваша логика для анализа строки расписания
-    // ...
-
-    return true; // Вернуть true, если работает сегодня и сейчас
-  }
-
-  sortTransportBySchedule(): void {
-    const { currentDay, currentTime } = this.getCurrentDayAndTimeInKZ();
-    
-    this.model.filteredTransport.sort((a, b) => {
-      const aOperating = this.isOperatingTodayAndNow(a.schedule, currentDay, currentTime) ? 1 : 0;
-      const bOperating = this.isOperatingTodayAndNow(b.schedule, currentDay, currentTime) ? 1 : 0;
-
-      return (bOperating - aOperating) * (this.isSortedInAscendingOrder ? 1 : -1);
-    });
-    
-    this.isSortedInAscendingOrder = !this.isSortedInAscendingOrder; // Переключение порядка сортировки
-  }
 }
-
