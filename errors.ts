cc95eeebf655f171c5e3706c3b4fb76a2446487b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 14:06:47 +0600

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -1,9 +1,29 @@
+{
+  "compilerOptions": {
+    "allowJs": true,
+    "composite": true,
+    "skipLibCheck": true,
+    "module": "ESNext",
+    "moduleResolution": "bundler",
+    "allowSyntheticDefaultImports": true
+  },
+  "include": ["vite.config.ts"]
+}
+
+
+
+
+
+
+
+
 PS C:\Users\ushakov.dmitriy\Desktop\Projects\alser.dispatcherworkplaceui\frontend> npm run build
 
 > alser.dispatcherworkplaceui@0.0.0 build
 > vue-tsc && vite build
 
-error TS6504: File 'C:/Users/ushakov.dmitriy/Desktop/Projects/alser.dispatcherworkplaceui/frontend/src/views/dashboard/dashboard.component.vue.js' is a JavaScript file. Did you mean to enable the 'allowJs' option?
+error TS6504: File 'C:/Users/ushakov.dmitriy/Desktop/Projects/alser.dispatcherworkplaceui/frontend/src/views/dashboard/dashboard.component.vue.js' is a JavaScript file. Did you mean to enable the 'allowJs
+' option?
   The file is in the program because:
     Root file specified for compilation
 
