9c9dcd2c2f53da80b9f1c644ebb52317cd88ea36
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 15:57:37 +0600

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -1,9 +1,34 @@
-PS C:\Users\ushakov.dmitriy\Desktop\Projects\alser.dispatcherworkplaceui> rm -rf backend/tms-api/.git
-Remove-Item : Не удается найти параметр, соответствующий имени параметра "rf".
-строка:1 знак:4
-+ rm -rf backend/tms-api/.git
-+    ~~~
-    + CategoryInfo          : InvalidArgument: (:) [Remove-Item], ParameterBindingException
-    + FullyQualifiedErrorId : NamedParameterNotFound,Microsoft.PowerShell.Commands.RemoveItemCommand
- 
-PS C:\Users\ushakov.dmitriy\Desktop\Projects\alser.dispatcherworkplaceui>
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> git add .
+error: 'backend/tms-api' does not have a commit checked out
+fatal: updating files failed
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> git status
+On branch develop-3
+Changes not staged for commit:
+  (use "git add/rm <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+  (commit or discard the untracked or modified content in submodules)
+        deleted:    .gitignore
+        deleted:    Dockerfile
+        deleted:    package-lock.json
+        deleted:    package.json
+        modified:   tms-api (untracked content)
+
+no changes added to commit (use "git add" and/or "git commit -a")
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> cd ..
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui> git add .
+error: 'backend/tms-api' does not have a commit checked out
+fatal: updating files failed
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui> git commit -m "nest added"
+On branch develop-3
+Changes not staged for commit:
+  (use "git add/rm <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+  (commit or discard the untracked or modified content in submodules)
+        deleted:    backend/.gitignore
+        deleted:    backend/Dockerfile
+        deleted:    backend/package-lock.json
+        deleted:    backend/package.json
+        modified:   backend/tms-api (untracked content)
+
+no changes added to commit (use "git add" and/or "git commit -a")
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui>
