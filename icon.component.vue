5e229a900df67eee6f1612d1651f0d87eacf31cb
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 16:37:26 +0600

    Update icon.component.vue

--- icon.component.vue
+++ icon.component.vue
@@ -1,3 +1,15 @@
 PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api> cd C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui
 PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui> git add .
 PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui> git commit -m "Added tsm-api without nested git repository"
+
+
+
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api> PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api> cd C:\Users\ushakov.dmitri
+y\Desktop\alser.dispatcherworkplaceui
+Get-Process : Не удается найти позиционный параметр, принимающий аргумент "cd".
+строка:1 знак:1
++ PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backe ...
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : InvalidArgument: (:) [Get-Process], ParameterBindingException
+    + FullyQualifiedErrorId : PositionalParameterNotFound,Microsoft.PowerShell.Commands.GetProcessCommand
+ 
