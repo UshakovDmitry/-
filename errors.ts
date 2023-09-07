da41422ab9bbc89adc1b990af07c8e73c43370ac
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 14:47:24 +0600

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -1,1 +1,9 @@
-rm -rf backend/tms-api/.git
+PS C:\Users\ushakov.dmitriy\Desktop\Projects\alser.dispatcherworkplaceui> rm -rf backend/tms-api/.git
+Remove-Item : Не удается найти параметр, соответствующий имени параметра "rf".
+строка:1 знак:4
++ rm -rf backend/tms-api/.git
++    ~~~
+    + CategoryInfo          : InvalidArgument: (:) [Remove-Item], ParameterBindingException
+    + FullyQualifiedErrorId : NamedParameterNotFound,Microsoft.PowerShell.Commands.RemoveItemCommand
+ 
+PS C:\Users\ushakov.dmitriy\Desktop\Projects\alser.dispatcherworkplaceui>
