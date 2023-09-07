2c515ef6db9cbf90db5ee8d23b835343e14bee55
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 16:34:03 +0600

    Update icon.component.vue

--- icon.component.vue
+++ icon.component.vue
@@ -1,8 +1,1 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> cd c:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api> ls -la
-Get-ChildItem : Не удается найти параметр, соответствующий имени параметра "la".
-строка:1 знак:4
-+ ls -la
-+    ~~~
-    + CategoryInfo          : InvalidArgument: (:) [Get-ChildItem], ParameterBindingException
-    + FullyQualifiedErrorId : NamedParameterNotFound,Microsoft.Powe
+Get-ChildItem -Force
