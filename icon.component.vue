6a2068dd64fa686f85111dff68570cb91fdcee7a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 16:33:29 +0600

    Update icon.component.vue

--- icon.component.vue
+++ icon.component.vue
@@ -1,74 +1,8 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npx @nestjs/cli new tsm-api
-Need to install the following packages:
-  @nestjs/cli
-Ok to proceed? (y) y
-⚡  We will scaffold your app in a few seconds..
-
-? Which package manager would you ❤️  to use? npm
-CREATE tsm-api/.eslintrc.js (663 bytes)
-CREATE tsm-api/.prettierrc (51 bytes)
-CREATE tsm-api/nest-cli.json (171 bytes)
-CREATE tsm-api/package.json (1948 bytes)
-CREATE tsm-api/README.md (3340 bytes)
-CREATE tsm-api/tsconfig.build.json (97 bytes)
-CREATE tsm-api/tsconfig.json (546 bytes)
-CREATE tsm-api/src/app.controller.spec.ts (617 bytes)
-CREATE tsm-api/src/app.controller.ts (274 bytes)
-CREATE tsm-api/src/app.module.ts (249 bytes)
-CREATE tsm-api/src/app.service.ts (142 bytes)
-CREATE tsm-api/src/main.ts (208 bytes)
-CREATE tsm-api/test/app.e2e-spec.ts (630 bytes)
-CREATE tsm-api/test/jest-e2e.json (183 bytes)
-
-✔ Installation in progress... ☕
-
-�  Successfully created project tsm-api
-�  Get started with the following commands:
-
-$ cd tsm-api
-$ npm run start
-
-
-                          Thanks for installing Nest �
-                 Please consider donating to our open collective
-                        to help us maintain this package.
-
-
-               �  Donate: https://opencollective.com/nest
-
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npx @nestjs/cli new tsm-api
-⚡  We will scaffold your app in a few seconds..
-
-? Which package manager would you ❤️  to use? npm
-CREATE tsm-api/.eslintrc.js (663 bytes)
-CREATE tsm-api/.prettierrc (51 bytes)
-CREATE tsm-api/nest-cli.json (171 bytes)
-CREATE tsm-api/package.json (1948 bytes)
-CREATE tsm-api/README.md (3340 bytes)
-CREATE tsm-api/tsconfig.build.json (97 bytes)
-CREATE tsm-api/tsconfig.json (546 bytes)
-CREATE tsm-api/src/app.controller.spec.ts (617 bytes)
-CREATE tsm-api/src/app.controller.ts (274 bytes)
-CREATE tsm-api/src/app.module.ts (249 bytes)
-CREATE tsm-api/src/app.service.ts (142 bytes)
-CREATE tsm-api/src/main.ts (208 bytes)
-CREATE tsm-api/test/app.e2e-spec.ts (630 bytes)
-CREATE tsm-api/test/jest-e2e.json (183 bytes)
-
-✔ Installation in progress... ☕
-
-�  Successfully created project tsm-api
-�  Get started with the following commands:
-
-$ cd tsm-api
-$ npm run start
-
-
-                          Thanks for installing Nest �
-                 Please consider donating to our open collective
-                        to help us maintain this package.
-
-
-               �  Donate: https://opencollective.com/nest
-
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> cd c:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api> ls -la
+Get-ChildItem : Не удается найти параметр, соответствующий имени параметра "la".
+строка:1 знак:4
++ ls -la
++    ~~~
+    + CategoryInfo          : InvalidArgument: (:) [Get-ChildItem], ParameterBindingException
+    + FullyQualifiedErrorId : NamedParameterNotFound,Microsoft.Powe
