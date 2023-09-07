42f825a430045d44a260af52d180ee0e7a0ac611
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 10:35:16 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -20,3 +20,23 @@ describe('Transport Tab Tests', () => {
     cy.get('[data-test="transport-btn"]').should('not.have.class', 'isActiveTab');
   });
 });
+//////////////
+describe('Search and Dropdown Tests', () => {
+  beforeEach(() => {
+    cy.visit('/transport');
+  });
+
+  it('should display a list of cities', () => {
+    cy.get('[data-test="dropdown-body"]').should('not.exist');
+    cy.get('[data-test="dropdown-btn"]').click();
+    cy.get('[data-test="dropdown-body"]').should('be.visible');
+  });
+
+  it('should display text', () => {
+    const numberPPO = 12345567;
+    cy.get('[data-test="search-input-PPO"]').should('be.visible');
+    cy.get('[data-test="search-input-PPO"]')
+      .should('be.visible')
+      .type(`${numberPPO}{enter}`);
+  });
+});
