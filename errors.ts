4ba07b7d1f2dc490ed12a21614dd2ac15bd78876
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 16:10:30 +0600

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -12,26 +12,16 @@ git commit -m "Added tms-api content directly into the repository"
 
 
 
-
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui> rm -rf .git/modules/backend/tms-api
-Remove-Item : Не удается найти параметр, соответствующий имени параметра "rf".
-строка:1 знак:4
-+ rm -rf .git/modules/backend/tms-api
-+    ~~~
-    + CategoryInfo          : InvalidArgument: (:) [Remove-Item], ParameterBindingException
-    + FullyQualifiedErrorId : NamedParameterNotFound,Microsoft.PowerShell.Commands.RemoveItemCommand
-
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui> Remove-Item -Recurse -Force .git\modules\backend\tms-api
->>
-Remove-Item : Не удается найти путь "C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\.git\modules\backend\tms-api", так как он не существует.
-строка:1 знак:1
-+ Remove-Item -Recurse -Force .git\modules\backend\tms-api
-+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    + CategoryInfo          : ObjectNotFound: (C:\Users\ushako...backend\tms-api:String) [Remove-Item], ItemNotFoundException
-    + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.RemoveItemCommand
-
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui> notepad .gitmodules
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui> git add .
-error: 'backend/tms-api/' does not have a commit checked out
-fatal: adding files failed
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui>
+[core]
+	repositoryformatversion = 0
+	filemode = false
+	bare = false
+	logallrefupdates = true
+	symlinks = false
+	ignorecase = true
+[remote "origin"]
+	url = http://bitbucket.next.local/scm/vue/alser.dispatcherworkplaceui.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+	remote = origin
+	merge = refs/heads/master
