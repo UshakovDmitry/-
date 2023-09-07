0e037d62a3eba2a32abe4dd82d5d1f337d1d7e73
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 14:02:04 +0600

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -1,1 +1,17 @@
-C:\Users\ushakov.dmitriy\AppData\Roaming\npm\nest.cmd new tms-api
+PS C:\Users\ushakov.dmitriy\Desktop\Projects\alser.dispatcherworkplaceui\frontend> npm run build
+
+> alser.dispatcherworkplaceui@0.0.0 build
+> vue-tsc && vite build
+
+error TS6504: File 'C:/Users/ushakov.dmitriy/Desktop/Projects/alser.dispatcherworkplaceui/frontend/src/views/dashboard/dashboard.component.vue.js' is a JavaScript file. Did you mean to enable the 'allowJs' option?
+  The file is in the program because:
+    Root file specified for compilation
+
+error TS6504: File 'C:/Users/ushakov.dmitriy/Desktop/Projects/alser.dispatcherworkplaceui/frontend/src/views/map/map.component.vue.js' is a JavaScript file. Did you mean to enable the 'allowJs' option?
+  The file is in the program because:
+    Root file specified for compilation
+
+
+Found 2 errors.
+
+PS C:\Users\ushakov.dmitriy\Desktop\Projects\alser.dispatcherworkplaceui\frontend>
