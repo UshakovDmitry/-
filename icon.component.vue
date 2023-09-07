b10d5e001b4e032ad5ca98acf62505c707845371
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 16:31:19 +0600

    Update icon.component.vue

--- icon.component.vue
+++ icon.component.vue
@@ -1,217 +1,74 @@
-<!-- eslint-disable camelcase -->
-<!-- eslint-disable camelcase -->
-<!-- eslint-disable camelcase -->
-<!-- eslint-disable camelcase -->
-<template>
-  <IconContainer></IconContainer>
-</template>
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npx @nestjs/cli new tsm-api
+Need to install the following packages:
+  @nestjs/cli
+Ok to proceed? (y) y
+⚡  We will scaffold your app in a few seconds..
 
-<script setup lang="ts">
-import { h, Component } from 'vue';
-import dashboardIcon from '../icons-collection/dashboard.icon.vue';
-import mapIcon from '../icons-collection/map.icon.vue';
-import assignmentIcon from '../icons-collection/assignment.icon.vue';
-import peopleIcon from '../icons-collection/people.icon.vue';
-import carIcon from '../icons-collection/car.icon.vue';
-import visibility from '../icons-collection/visibility.icon.vue';
-import visibility_off from '../icons-collection/visibility_off.icon.vue';
-import location from '../icons-collection/location.icon.vue';
-import expand_more from '../icons-collection/expand-more.icon.vue';
-import search from '../icons-collection/search.icon.vue';
-import keyboardLeft from '../icons-collection/keyboard-left.icon.vue';
-import keyboardRight from '../icons-collection/keyboard-right.icon.vue';
-import doubleArrowLeft from '../icons-collection/double-arrow-left.icon.vue';
-import doubleArrowRight from '../icons-collection/double-arrow-right.icon.vue';
-import sortIcon from '../icons-collection/sort.icon.vue';
+? Which package manager would you ❤️  to use? npm
+CREATE tsm-api/.eslintrc.js (663 bytes)
+CREATE tsm-api/.prettierrc (51 bytes)
+CREATE tsm-api/nest-cli.json (171 bytes)
+CREATE tsm-api/package.json (1948 bytes)
+CREATE tsm-api/README.md (3340 bytes)
+CREATE tsm-api/tsconfig.build.json (97 bytes)
+CREATE tsm-api/tsconfig.json (546 bytes)
+CREATE tsm-api/src/app.controller.spec.ts (617 bytes)
+CREATE tsm-api/src/app.controller.ts (274 bytes)
+CREATE tsm-api/src/app.module.ts (249 bytes)
+CREATE tsm-api/src/app.service.ts (142 bytes)
+CREATE tsm-api/src/main.ts (208 bytes)
+CREATE tsm-api/test/app.e2e-spec.ts (630 bytes)
+CREATE tsm-api/test/jest-e2e.json (183 bytes)
 
-export interface IIcon {
-  name: string;
-  color: string;
-  colorLoading?: string;
-  width?: number;
-  height?: number;
-  scale?: number;
-  isDisabled?: boolean;
-  isLoading?: boolean;
-  viewBox?: string | undefined;
-}
+✔ Installation in progress... ☕
 
-const props = defineProps<{
-  сonfig: IIcon;
-}>();
+�  Successfully created project tsm-api
+�  Get started with the following commands:
 
-const emits = defineEmits(['onClick']);
-
-const listIcons: { [key: string]: any } = {
-  //Navbar
-  dashboardIcon,
-  mapIcon,
-  assignmentIcon,
-  peopleIcon,
-  carIcon,
-  //for fields
-  visibility,
-  visibility_off,
-  location,
-  // dropdown стрелка вниз
-  expand_more,
-  search,
-  // paginator
-  keyboardLeft,
-  keyboardRight,
-  doubleArrowLeft,
-  doubleArrowRight,
-  sortIcon
-
-};
-
-const IconContainer: Component = () => {
-  return h(
-    'svg',
-    {
-      xmlns: 'http://www.w3.org/2000/svg',
-      role: 'presentation',
-      width: props.сonfig?.width || 18,
-      height: props.сonfig?.height || 18,
-      viewBox: props.сonfig?.viewBox || '0 0 24 24',
-      style: `transform: scale(${props.сonfig?.scale})`,
-      onClick: () => {
-        emits('onClick');
-      },
-    },
-    [
-      h('title', { lang: 'en' }),
-      h(
-        'g',
-        {
-          fill: props.сonfig?.isDisabled
-            ? `var(--dark-30)`
-            : props.сonfig?.isLoading
-            ? props.сonfig?.colorLoading
-              ? `var(--${props.сonfig?.colorLoading})`
-              : 'transparent'
-            : props.сonfig?.color,
-        },
-        h(listIcons[props.сonfig.name]),
-      ),
-    ],
-  );
-};
-</script>
+$ cd tsm-api
+$ npm run start
 
 
+                          Thanks for installing Nest �
+                 Please consider donating to our open collective
+                        to help us maintain this package.
 
 
+               �  Donate: https://opencollective.com/nest
+
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npx @nestjs/cli new tsm-api
+⚡  We will scaffold your app in a few seconds..
+
+? Which package manager would you ❤️  to use? npm
+CREATE tsm-api/.eslintrc.js (663 bytes)
+CREATE tsm-api/.prettierrc (51 bytes)
+CREATE tsm-api/nest-cli.json (171 bytes)
+CREATE tsm-api/package.json (1948 bytes)
+CREATE tsm-api/README.md (3340 bytes)
+CREATE tsm-api/tsconfig.build.json (97 bytes)
+CREATE tsm-api/tsconfig.json (546 bytes)
+CREATE tsm-api/src/app.controller.spec.ts (617 bytes)
+CREATE tsm-api/src/app.controller.ts (274 bytes)
+CREATE tsm-api/src/app.module.ts (249 bytes)
+CREATE tsm-api/src/app.service.ts (142 bytes)
+CREATE tsm-api/src/main.ts (208 bytes)
+CREATE tsm-api/test/app.e2e-spec.ts (630 bytes)
+CREATE tsm-api/test/jest-e2e.json (183 bytes)
+
+✔ Installation in progress... ☕
+
+�  Successfully created project tsm-api
+�  Get started with the following commands:
+
+$ cd tsm-api
+$ npm run start
 
 
-
-//// with rotate
-
-
-<template>
-  <IconContainer></IconContainer>
-</template>
-
-<script setup lang="ts">
-import { h, Component } from 'vue';
-// ... (ваши импорты)
-
-export interface IIcon {
-  name: string;
-  color: string;
-  colorLoading?: string;
-  width?: number;
-  height?: number;
-  scale?: number;
-  isDisabled?: boolean;
-  isLoading?: boolean;
-  viewBox?: string | undefined;
-  rotate?: number; // Добавьте эту строку
-}
-
-const props = defineProps<{
-  сonfig: IIcon;
-}>();
-
-const emits = defineEmits(['onClick']);
-
-// ... (список иконок)
-
-const IconContainer: Component = () => {
-  return h(
-    'svg',
-    {
-      xmlns: 'http://www.w3.org/2000/svg',
-      role: 'presentation',
-      width: props.сonfig?.width || 18,
-      height: props.сonfig?.height || 18,
-      viewBox: props.сonfig?.viewBox || '0 0 24 24',
-      style: `
-        transform: scale(${props.сonfig?.scale}) rotate(${props.сonfig?.rotate || 0}deg);
-      `, // Добавьте rotate здесь
-      onClick: () => {
-        emits('onClick');
-      },
-    },
-    [
-      h('title', { lang: 'en' }),
-      h(
-        'g',
-        {
-          fill: props.сonfig?.isDisabled
-            ? `var(--dark-30)`
-            : props.сonfig?.isLoading
-            ? props.сonfig?.colorLoading
-              ? `var(--${props.сonfig?.colorLoading})`
-              : 'transparent'
-            : props.сonfig?.color,
-        },
-        h(listIcons[props.сonfig.name]),
-      ),
-    ],
-  );
-};
-</script>
+                          Thanks for installing Nest �
+                 Please consider donating to our open collective
+                        to help us maintain this package.
 
 
+               �  Donate: https://opencollective.com/nest
 
-
-const IconContainer: Component = () => {
-  return h(
-    'svg',
-    {
-      xmlns: 'http://www.w3.org/2000/svg',
-      role: 'presentation',
-      width: props.сonfig?.width || 18,
-      height: props.сonfig?.height || 18,
-      viewBox: props.сonfig?.viewBox || '0 0 24 24',
-      style: {
-        transform: `scale(${props.сonfig?.scale}) rotate(${props.сonfig?.rotate || 0}deg)`,
-        transformOrigin: 'center'
-      },
-      onClick: () => {
-        emits('onClick');
-      },
-    },
-    // ... остальной код
-  );
-};
-
-
-
-
-
-
-
-
-
-
-  <IconComponent
-    :сonfig="{
-      name: 'doubleArrowRight',
-      color: '#23362D4D',
-      width: 34,
-      height: 34,
-      rotate: 90,
-    }"
-  ></IconComponent>
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
