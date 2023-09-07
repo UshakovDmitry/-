93e7c129b4281f0c71e29fdb9dd856c7b9165a5d
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 16:06:01 +0600

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -4,6 +4,9 @@ git rm --cached backend/tms-api
 
 rm -rf .git/modules/backend/tms-api
 
+Remove-Item -Recurse -Force .git\modules\backend\tms-api
+
+
 git add backend/tms-api
 git commit -m "Added tms-api content directly into the repository"
 
