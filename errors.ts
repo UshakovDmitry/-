108edf19e18488dc364f57dba043c1f0c6d458ea
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 12:36:00 +0600

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -1,4 +1,1 @@
-PS C:\Users\ushakov.dmitriy\Desktop\Projects\alser.dispatcherworkplaceui\backend> npm bin -g
 C:\Users\ushakov.dmitriy\AppData\Roaming\npm
-(not in PATH env variable)
-PS C:\Users\ushakov.dmitriy\Desktop\Projects\alser.dispatcherworkplaceui\backend>
