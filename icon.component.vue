6c9242ae5bf83ca1ae495c47d21bf145b7d70d99
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 16:35:48 +0600

    Update icon.component.vue

--- icon.component.vue
+++ icon.component.vue
@@ -1,1 +1,47 @@
-Remove-Item -Recurse -Force .git
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api> Get-ChildItem -Force
+
+
+    Каталог: C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api
+
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+d--h--       07.09.2023     16:28                .git
+d-----       07.09.2023     16:28                node_modules
+d-----       07.09.2023     16:26                src
+d-----       07.09.2023     16:26                test
+-a----       07.09.2023     16:26            663 .eslintrc.js
+-a----       07.09.2023     16:28            391 .gitignore
+-a----       07.09.2023     16:26             51 .prettierrc
+-a----       07.09.2023     16:26            171 nest-cli.json
+-a----       07.09.2023     16:28         564721 package-lock.json
+-a----       07.09.2023     16:26           1948 package.json
+-a----       07.09.2023     16:26           3340 README.md
+-a----       07.09.2023     16:26             97 tsconfig.build.json
+-a----       07.09.2023     16:26            546 tsconfig.json
+
+
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api> Remove-Item -Recurse -Force .git
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api> Get-ChildItem -Force
+
+
+    Каталог: C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api
+
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+d-----       07.09.2023     16:28                node_modules
+d-----       07.09.2023     16:26                src
+d-----       07.09.2023     16:26                test
+-a----       07.09.2023     16:26            663 .eslintrc.js
+-a----       07.09.2023     16:28            391 .gitignore
+-a----       07.09.2023     16:26             51 .prettierrc
+-a----       07.09.2023     16:26            171 nest-cli.json
+-a----       07.09.2023     16:28         564721 package-lock.json
+-a----       07.09.2023     16:26           1948 package.json
+-a----       07.09.2023     16:26           3340 README.md
+-a----       07.09.2023     16:26             97 tsconfig.build.json
+-a----       07.09.2023     16:26            546 tsconfig.json
+
+
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api>
