77c2934a3139b1862323100220f9868917b993f3
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 14:12:40 +0600

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -1,37 +1,3 @@
 {
-  "compilerOptions": {
-    "allowJs": true,
-    "composite": true,
-    "skipLibCheck": true,
-    "module": "ESNext",
-    "moduleResolution": "bundler",
-    "allowSyntheticDefaultImports": true
-  },
-  "include": ["vite.config.ts"]
+  "exclude": ["node_modules", "src/**/*.vue.js"]
 }
-
-
-
-
-
-
-
-
-PS C:\Users\ushakov.dmitriy\Desktop\Projects\alser.dispatcherworkplaceui\frontend> npm run build
-
-> alser.dispatcherworkplaceui@0.0.0 build
-> vue-tsc && vite build
-
-error TS6504: File 'C:/Users/ushakov.dmitriy/Desktop/Projects/alser.dispatcherworkplaceui/frontend/src/views/dashboard/dashboard.component.vue.js' is a JavaScript file. Did you mean to enable the 'allowJs
-' option?
-  The file is in the program because:
-    Root file specified for compilation
-
-error TS6504: File 'C:/Users/ushakov.dmitriy/Desktop/Projects/alser.dispatcherworkplaceui/frontend/src/views/map/map.component.vue.js' is a JavaScript file. Did you mean to enable the 'allowJs' option?
-  The file is in the program because:
-    Root file specified for compilation
-
-
-Found 2 errors.
-
-PS C:\Users\ushakov.dmitriy\Desktop\Projects\alser.dispatcherworkplaceui\frontend>
