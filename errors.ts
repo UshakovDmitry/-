9a8844b1d5b371b51ef60862491aee8be58db8fe
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-07 16:17:40 +0600

    Update errors.ts

--- errors.ts
+++ errors.ts
@@ -1,29 +1,4 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui> cd c:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> nest new tsm-backend
-nest : Имя "nest" не распознано как имя командлета, функции, файла сценария или выполняемой программы. Проверьте правильность написания имени, а также наличие и правильность пути, после ч
-его повторите попытку.
-строка:1 знак:1
-+ nest new tsm-backend
-+ ~~~~
-    + CategoryInfo          : ObjectNotFound: (nest:String) [], CommandNotFoundException
-    + FullyQualifiedErrorId : CommandNotFoundException
- 
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>  npm i -g @nestjs/cli
+nest --version
 
-changed 263 packages, and audited 264 packages in 33s
 
-44 packages are looking for funding
-  run `npm fund` for details
-
-found 0 vulnerabilities
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> nest new tsm-api
-nest : Имя "nest" не распознано как имя командлета, функции, файла сценария или выполняемой программы. Проверьте правильность написания имени, а также наличие и правильность пути, после ч
-его повторите попытку.
-строка:1 знак:1
-+ nest new tsm-api
-+ ~~~~
-    + CategoryInfo          : ObjectNotFound: (nest:String) [], CommandNotFoundException
-    + FullyQualifiedErrorId : CommandNotFoundException
- 
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
-	
+npx @nestjs/cli new tsm-api
